2aa41a4939406180767c19770d3518a6678d7512
BlockChainPeople <168885157+BlockChainPeople@users.noreply.github.com>
2024-05-17 00:42:10 +0800

    Add files via upload

--- static/js/wow.js
+++ static/js/wow.js
@@ -1,14 +1,24 @@
 /* Animation */
 
-wow = new WOW(
-  {
-      boxClass:     'wow',      // default
-      animateClass: 'animated', // change this if you are not using animate.css
-      offset:       0,          // default
-      mobile:       true,       // keep it on mobile
-      live:         true        // track if element updates
-    }
-  )
- wow.init();
+wow = new WOW({
+  boxClass: "wow", // default
+  animateClass: "animated", // change this if you are not using animate.css
+  offset: 0, // default
+  mobile: true, // keep it on mobile
+  live: true, // track if element updates
+});
+wow.init();
+
+$(function () {
+  var clipboard=new ClipboardJS("#JQ_copy", {
+    text: function (trigger) {
+      return $("#JQ_address").text();
+    },
+  });
+  clipboard.on('success', function (e) {
+    $('.JQ_toast').toast("show")
+  });
+
+});
 
 /* Animation */
